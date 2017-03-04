826e4ae9c52ef3e6a4f302a493a4307a7c2bdbb3
juhyun10 <juhyun1010@naver.com>
2017-03-04 14:34:31 +0900

    기본적인 npm 명령어와 save 옵션

--- chap07/7-3-1.supervisor.js
+++ chap07/7-3-1.supervisor.js
@@ -7,7 +7,7 @@ var http = require('http');
 
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<h1>Test - File - 2</h1>');
+    res.end('<h1>Test - File - 3</h1>');
 }).listen(52273, function() {
     console.log('Server Running at http://127.0.0.1:52273.');
 });
