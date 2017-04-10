66ba05a0996f6ea0f25d524e8e586053e3b356c6
juhyun10 <juhyun1010@naver.com>
2017-04-10 14:59:15 +0900

    chap11.socket.io 모듈 - 웹에서 모바일채팅 구현 + 대화내용저장 + 대화방생성 및 참여

--- chap11/11-4-1.socket.chatting.js
+++ chap11/11-4-1.socket.chatting.js
@@ -7,6 +7,14 @@ DATE: 2017.04.05
 var http = require('http');
 var fs = require('fs');
 var socketio = require('socket.io');
+var mysql = require('mysql');
+
+// 데이터베이스와 연결
+var client = mysql.createConnection({
+    user:'root',
+    password:'1234',
+    database:'company'
+});
 
 // http 모듈 이용하여 웹서버 생성
 var server = http.createServer(function(req, res) {
@@ -21,9 +29,27 @@ var server = http.createServer(function(req, res) {
 // socket.io 모듈로 소켓서버 생성
 var io = socketio.listen(server);
 io.sockets.on('connection', function(socket) {
+    // 방이름
+    var roomNm = null;
+
+    // join 이벤트 (클라이언트가 특정한 방에 접속하게 만드는 이벤트)
+    // 방에 접속시키고 현재 방의 이름을 리턴
+    socket.on('join', function(data) {
+        roomNm = data;
+        socket.join(roomNm, function() {
+            console.log('socket.rooms : ', socket.rooms);
+            io.sockets.in(roomNm).emit('moveChat', roomNm);
+        });
+    });
+
     // messageAssu 이벤트
     socket.on('messageAssu', function(data) {
+        // 대화내용 저장
+        client.query('INSERT INTO CHAT (ROOMNM, NAME, MSG, DATE) VALUES (?, ?, ?, ?)', [
+            data.roomNm, data.name, data.message, new Date()
+        ]);
+
         // 현재 접속해있는 모든 소켓에게 이벤트 전달 ()클라이언트의 messageAssu 이벤트 발생새킴)
-        io.sockets.emit('messageAssu', data);
+        io.sockets.in(data.roomNm).emit('messageAssu', data);
     });
 });
